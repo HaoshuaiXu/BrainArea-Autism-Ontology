ff73f40238a364e2104f54de3f61dc8776e60938
StevenXu <hsh.xu@outlook.com>
2020-12-17 14:47:47 +0800

    Remove txt proceed code

--- read_sentence.py
+++
@@ -1,5 +0,0 @@
-def get_sentence_data(txt_filepath) -> list:
-    with open(txt_filepath, 'r', encoding='utf-8') as f:
-        sentence = [s.strip('\n') for s in f.readlines()]
-    return sentence
-
